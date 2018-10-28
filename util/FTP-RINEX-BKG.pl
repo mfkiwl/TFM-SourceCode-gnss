7f06a6a03236793ed4d6928fcec200c41572f083
ppinto94 <pintosantospablo@gmail.com>
2018-10-28 10:22:18 +0100

    Adding an early version of util scripts. This will act as a reference version for future work.

---
+++ util/FTP-RINEX-BKG.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+
+use feature qw(say);
+use Data::Dumper;
+
+# ---------------------------------------------------------------------------- #
+# SCRIPT:
+# Download RINEX v303 from Bundesamt für Kartographie und Geodäsie (BKG)
+# file repository.
+# ---------------------------------------------------------------------------- #
+
+# Script arguments:
+# - $1 -> Year
+# - $2 -> Day of year
+# - $3 -> file containing the stations
+# - $4 -> storage path
+
+my ($year, $doy_raw, $stations_file_path, $storage_path) = @ARGV;
+
+# ---------------------------------------------------------------------------- #
+
+# Main variables:
+my $yy  = substr($year, 2, 2);       # last two digits of year...
+my $doy = sprintf("%03d", $doy_raw); # day of year (3 leading zeros format)...
+my $yyo = $yy."o";                   # target rinex extension...
+
+# CDDIS ftp path:
+my $ftp_parent = qw(ftp://igs.bkg.bund.de/IGS/obs/);
+my $ftp_rinex = $ftp_parent."$year/$doy/";
+
+
+# Get station list:
+# ---------------------------------------------------------------------------- #
+# Open stations file:
+my $fh; open($fh, "<", $stations_file_path) or die $!;
+
+# Store file content in internal variable:
+my $stations_content;
+while (my $line = <$fh>) { $stations_content.= $line; }
+
+# Close stations file:
+close($fh);
+
+# Split station content into internal array:
+# Content is split by any combination of: \n, \t, ' ', ','...
+my @stations = split(/\s+|,\s*/,  $stations_content);
+
+# Stations names to upper case:
+@stations = map(uc, @stations);
+
+
+# Download RINEXs:
+# ---------------------------------------------------------------------------- #
+my %rinex_list;
+
+foreach my $station (@stations) {
+  # Get RINEX name:
+  my $rinex      = $station.'*'.$year.$doy.'*O.crx.gz';
+  my $rinex_url  = $ftp_rinex.$rinex;
+
+  # Download RINEX using bash 'wget' command:
+  qx(wget $rinex_url);
+
+  # Fill file status hash:
+  $rinex_list{$station} = (-e $rinex) ? qw(DOWNLOADED) : qw(NOT DOWNLOADED);
+
+  # Move the downloaded files to storage path if defined:
+  qx(mv $rinex $storage_path) if $storage_path;
+}
+
+# Print the status of the requested files:
+say ""; print Dumper \%rinex_list; say "";
+
+# Print the storage output if defined:
+say " > Downloaded RINEX are available in $storage_path \n" if $storage_path;
