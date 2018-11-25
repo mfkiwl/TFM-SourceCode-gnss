4c6c8bebc9065f72a4017e6c87343e16a7879df6
unknown <pintosantospablo@gmail.com>
2018-11-25 13:21:13 +0100

    Including navigation rinex file download from BKG server

--- util/FTP-RINEX-BKG.pl
+++ util/FTP-RINEX-BKG.pl
@@ -2,7 +2,6 @@
 
 use strict;
 use warnings;
-use diagnostics;
 
 use feature qw(say);
 use Data::Dumper;
@@ -29,7 +28,7 @@ my $doy = sprintf("%03d", $doy_raw); # day of year (3 leading zeros format)...
 my $yyo = $yy."o";                   # target rinex extension...
 
 # CDDIS ftp path:
-my $ftp_parent = qw(ftp://igs.bkg.bund.de/IGS/obs/);
+my $ftp_parent = qq(ftp://igs.bkg.bund.de/IGS/obs/);
 my $ftp_rinex = $ftp_parent."$year/$doy/";
 
 
@@ -47,17 +46,17 @@ close($fh);
 
 # Split station content into internal array:
 # Content is split by any combination of: \n, \t, ' ', ','...
-my @stations = split(/\s+|,\s*/,  $stations_content);
+my @stations_list = split(/\s+|,\s*/,  $stations_content);
 
 # Stations names to upper case:
-@stations = map(uc, @stations);
+@stations_list = map(uc, @stations_list);
 
 
-# Download RINEXs:
+# Download Observation RINEXs:
 # ---------------------------------------------------------------------------- #
 my %rinex_list;
 
-foreach my $station (@stations) {
+foreach my $station (@stations_list) {
   # Get RINEX name:
   my $rinex      = $station.'*'.$year.$doy.'*O.crx.gz';
   my $rinex_url  = $ftp_rinex.$rinex;
@@ -66,10 +65,52 @@ foreach my $station (@stations) {
   qx(wget $rinex_url);
 
   # Fill file status hash:
-  $rinex_list{$station} = (-e $rinex) ? qw(DOWNLOADED) : qw(NOT DOWNLOADED);
+  $rinex_list{$station."-OBS"} = (-e $rinex) ? qw(DOWNLOADED) : qw(NOT DOWNLOADED);
 
   # Move the downloaded files to storage path if defined:
   qx(mv $rinex $storage_path) if $storage_path;
+}
+
+
+# Download Navigation RINEXs:
+# ---------------------------------------------------------------------------- #
+my $nav_file_input = '';
+
+until ($nav_file_input =~ /yes/i || $nav_file_input =~ /no/i) {
+  say "Would you like to download navigation files as well? (yes/no)";
+  $nav_file_input = <STDIN>; chomp $nav_file_input;
+}
+
+if ( $nav_file_input =~ /yes/i )
+{
+  # Ask for satellite systems:
+  say "Please provide the constellations from which you would like the ".
+      "navigation data (G/E/R): ";
+  my $sat_sys_input = <STDIN>; chomp $sat_sys_input;
+
+  # Get satellite system array list:
+  my @sat_sys_list = split(/[\s,;]+/, $sat_sys_input);
+
+  # Iterate over satellite system and station:
+  for my $sat_sys (@sat_sys_list) {
+    for my $station (@stations_list) {
+      my $rinex      = $station.'*'.$year.$doy.'*'.$sat_sys.'N.rnx.gz';
+      my $rinex_url  = $ftp_rinex.$rinex;
+
+      # Download RINEX using bash 'wget' command:
+      qx(wget $rinex_url);
+
+      # Fill file status hash:
+      $rinex_list{join('-', ($station, $sat_sys, "NAV"))} =
+        (-e $rinex) ? qw(DOWNLOADED) : qw(NOT DOWNLOADED);
+
+      # Move the downloaded files to storage path if defined:
+      qx(mv $rinex $storage_path) if $storage_path;
+    }
+  }
+
+} else {
+
 }
 
 # Print the status of the requested files:
