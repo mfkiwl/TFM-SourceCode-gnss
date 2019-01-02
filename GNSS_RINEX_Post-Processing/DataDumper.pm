dda4fc9dfb03a3977b311baff078a4d5a4727e3e
unknown <pintosantospablo@gmail.com>
2019-01-02 23:02:38 +0100

    Developing DataDumper module. All subs are finished. Pending TODO activities!

--- GNSS_RINEX_Post-Processing/DataDumper.pm
+++ GNSS_RINEX_Post-Processing/DataDumper.pm
@@ -54,7 +54,7 @@ BEGIN {
   our @EXPORT_CONST = qw(  );
 
   # Define subroutines to export:
-  our @EXPORT_SUB   = qw( &DumpLSQInfo
+  our @EXPORT_SUB   = qw( &DumpLSQReport
                           &DumpSatObsData
                           &DumpSatPosition
                           &DumpRecPosition
@@ -137,9 +137,9 @@ sub DumpSatObsData {
     return KILLED;
   }
 
-  # ***************************************** #
-  # Satellite Observations data dump routine: #
-  # ***************************************** #
+  # ******************************************* #
+  # Satellite Observations data dumper routine: #
+  # ******************************************* #
 
   # De-reference array inputs:
   my @selected_obs   = @{ $ref_selected_obs   };
@@ -183,36 +183,44 @@ sub DumpSatObsData {
       }
 
       # Header line items:
-      my @header_items = qw(Epoch Status Sat_PRN);
+      my @header_items = qw(Epoch Status SatID);
       push(@header_items, "$_") for (@sat_sys_obs);
 
       # Write header:
-      say $fh "# ".join($separator, @header_items);
+      say $fh "#".join($separator, @header_items);
 
     # 4. Dump satellite observations:
       for (my $i = 0; $i < scalar(@{$ref_obs_data->{BODY}}); $i += 1)
       {
         # Save epoch data reference:
-        my $ref_epoch_data = $ref_obs_data->{BODY}[$i];
+        my $ref_epoch_data =
+           $ref_obs_data->{BODY}[$i];
 
         # Save observation epoch status:
-        my $status = $ref_epoch_data->{STATUS};
+        my $status =
+           $ref_epoch_data->{STATUS};
 
-        # Epoch is transformed according to data dumper configuration:
+        # Epoch is transformed according to configuration:
         my @epoch =
-          &{$ref_dump_conf->{EPOCH_FORMAT}}( $ref_epoch_data->{EPOCH} );
+           &{$ref_dump_conf->{EPOCH_FORMAT}}( $ref_epoch_data->{EPOCH} );
 
-        # Write observations for each observed satellite:
-        for my $sat (sort ( keys %{$ref_epoch_data->{SAT_OBS}} ))
-        {
+        # Write observation data:
+        for my $sat (sort ( keys %{$ref_epoch_data->{SAT_OBS}} )) {
           unless (grep(/^$sat$/, @sats_to_ignore)) {
-            # Save raw observations:
-            my @obs;
-            push(@obs, $ref_epoch_data->{SAT_OBS}{$sat}{$_}) for (@sat_sys_obs);
+
+            # Set line elements:
+            my @line_items = (@epoch, $status, $sat);
+
+            # Include selected observations:
+            push(@line_items,
+                 $ref_epoch_data->{SAT_OBS}{$sat}{$_}) for (@sat_sys_obs);
+
             # Dump observation data:
-            say $fh join($separator, (@epoch, $status, $sat, @obs));
-          }
+            say $fh join($separator, @line_items);
+
+          } # end unless $sat
         } # end for my $sat
+
       } # end for $i
 
     # 5. Close dumper file:
@@ -220,22 +228,479 @@ sub DumpSatObsData {
 
   } # end for $sat_sys
 
-  # If successfull, the sub returns boolean TRUE answer:
+  # Sub's answer is TRUE if successfull:
   return TRUE;
 }
 
 sub DumpRecSatLoSData {
-  my ($ref_dump_conf, $ref_gen_conf, $ref_obs_data,
-      $ref_sats_to_ignore, ) = @_;
+  my ($ref_dump_conf, $ref_gen_conf,
+      $ref_obs_data, $ref_sats_to_ignore, $output_path, $fh_log) = @_;
 
+  # Default input values if not defined:
+  $fh_log = *STDOUT unless $fh_log;
 
+  # ************************* #
+  # Input consistency cehcks: #
+  # ************************* #
+
+  # Output path must exist and have write permissions:
+  unless (-w $output_path) {
+    RaiseError($fh_log, ERR_WRITE_PERMISSION_DENIED,
+      "User '".$ENV{USER}."' does not have write permissions at $output_path");
+    return KILLED;
+  }
+
+  # Dumper configuration must be hash type:
+  unless (ref($ref_dump_conf) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_dump_conf\' is not HASH type");
+    return KILLED;
+  }
+
+  # General configuration must be hash type:
+  unless (ref($ref_gen_conf) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_gen_conf\' is not HASH type");
+    return KILLED;
+  }
+
+  # Observation data must be hash type:
+  unless (ref($ref_obs_data) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_obs_data\' is not HASH type");
+    return KILLED;
+  }
+
+  # Satellites to discard must be array type:
+  unless (ref($ref_sats_to_ignore) eq 'ARRAY') {
+    RaiseError($fh_log, ERR_WRONG_ARRAY_REF,
+      "Input argument \'$ref_sats_to_ignore\' is not ARRAY type");
+    return KILLED;
+  }
+
+  # ************************************** #
+  # Receiver-Satellite LoS dumper routine: #
+  # ************************************** #
+
+  # De-reference array inputs:
+  my @sats_to_ignore = @{ $ref_sats_to_ignore };
+
+  # Save dumper useful configuration:
+  my $separator     = $ref_dump_conf->{ SEPARATOR    };
+  my $ref_epoch_sub = $ref_dump_conf->{ EPOCH_FORMAT };
+  my $ref_angle_sub = $ref_dump_conf->{ ANGLE_FORMAT };
+
+  # Create file for each selected constellation:
+  for my $sat_sys (@{ $ref_gen_conf->{SELECTED_SAT_SYS} })
+  {
+    # 1. Open dumper file at output path:
+      my $file_path = join('/', ($output_path, "$sat_sys-los-data.out"));
+      my $fh; open($fh, '>', $file_path) or croak "Could not create $!";
+
+    # 2. Write title line:
+      say $fh sprintf("# > Receiver-Satellite LoS Data. Created  : %s \n".
+                      "#   Reference system for ECEF coordinates : %s \n",
+                      GetPrettyLocalDate(),
+                      $ref_gen_conf->{ELIPSOID});
+
+    # 3. Write header line:
+      my @header_items = qw( Epoch Status SatID TropoCorr IonoCorr
+                             Azimut Zenital Elevation Distance IX IY IZ );
+
+      say $fh "#".join($separator, @header_items);
+
+    # 4. Write Line of Sight data:
+      # Go through the observations epochs:
+      for (my $i = 0; $i < scalar(@{ $ref_obs_data->{BODY} }); $i += 1)
+      {
+        # Save observation epoch status:
+        my $status =
+           $ref_obs_data->{BODY}[$i]{STATUS};
+        # Epoch is transformed according to configuration:
+        my @epoch =
+           &{ $ref_epoch_sub }( $ref_obs_data->{BODY}[$i]{EPOCH} );
+
+        # Go through available satellites in LoS-Data hash:
+        for my $sat (keys %{ $ref_obs_data->{BODY}[$i]{SAT_LOS} })
+        {
+          # Discard specified satellites:
+          unless (grep(/^$sat$/, @sats_to_ignore))
+          {
+            # Save LoS data reference:
+            my $ref_sat_los_data = $ref_obs_data->{BODY}[$i]{SAT_LOS}{$sat};
+
+            # Angle data is transformed according to configuration:
+            my @angle_data =
+              &{ $ref_angle_sub }( $ref_sat_los_data->{ AZIMUT    },
+                                   $ref_sat_los_data->{ ZENITAL   },
+                                   $ref_sat_los_data->{ ELEVATION } );
+
+            # Save line items:
+            # TODO: indicate reference system for ECEF_VECTOR?
+            my @line_items = ( @epoch, $status, $sat,
+                               $ref_sat_los_data->{ TROPO_CORR },
+                               $ref_sat_los_data->{ IONO_CORR  },
+                               @angle_data,
+                               $ref_sat_los_data->{ DISTANCE    },
+                               $ref_sat_los_data->{ ECEF_VECTOR }[0],
+                               $ref_sat_los_data->{ ECEF_VECTOR }[1],
+                               $ref_sat_los_data->{ ECEF_VECTOR }[2] );
+
+            # Write line:
+            say $fh join($separator, @line_items);
+
+          } # end unless $sat
+        } # end for $sat
+      } # end for $i
+
+    # 5. Close dumper file:
+    close($fh);
+
+  } # end for $sat_sys
+
+  # Sub's answer is TRUE if successfull:
+  return TRUE;
 }
 
-sub DumpLSQInfo {}
+sub DumpLSQReport {
+  my ( $ref_dump_conf, $ref_gen_conf,
+       $ref_obs_data, $output_path, $fh_log ) = @_;
 
-sub DumpSatPosition {}
+  # Default input values if not defined:
+  $fh_log = *STDOUT unless $fh_log;
 
-sub DumpRecPosition {}
+  # ************************* #
+  # Input consistency cehcks: #
+  # ************************* #
+
+  # Output path must exist and have write permissions:
+  unless (-w $output_path) {
+    RaiseError($fh_log, ERR_WRITE_PERMISSION_DENIED,
+      "User '".$ENV{USER}."' does not have write permissions at $output_path");
+    return KILLED;
+  }
+
+  # Dumper configuration must be hash type:
+  unless (ref($ref_dump_conf) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_dump_conf\' is not HASH type");
+    return KILLED;
+  }
+
+  # General configuration must be hash type:
+  unless (ref($ref_gen_conf) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_gen_conf\' is not HASH type");
+    return KILLED;
+  }
+
+  # Observation data must be hash type:
+  unless (ref($ref_obs_data) eq 'HASH') {
+    RaiseError($fh_log, ERR_WRONG_HASH_REF,
+      "Input argument \'$ref_obs_data\' is not HASH type");
+    return KILLED;
+  }
+
+  # ************************************** #
+  # Receiver-Satellite LoS dumper routine: #
+  # ************************************** #
+
+  # Save dumper useful configuration:
+  my $separator     = $ref_dump_conf->{ SEPARATOR    };
+  my $ref_epoch_sub = $ref_dump_conf->{ EPOCH_FORMAT };
+
+  # 1. Open dumper file at output path:
+    my $file_path = join('/', ($output_path, "lsq_report_info.out"));
+    my $fh; open($fh, '>', $file_path) or croak "Could not create $!";
+
+  # 2. Write title line:
+    say $fh sprintf("# > Least Squares Report. Created : %s \n",
+                    GetPrettyLocalDate());
+
+  # 3. Write header line:
+    my @header_items = qw( Epoch Iteration Status StdDevEstimator
+                           ConvergenceFlag );
+
+    # Insert number of apx parameters:
+    push(@header_items,
+         "ApprxParameter[$_]") for (0..NUM_PARAMETERS_TO_ESTIMATE - 1);
+    push(@header_items,
+         "DeltaParameter[$_]") for (0..NUM_PARAMETERS_TO_ESTIMATE - 1);
+
+    say $fh "#".join($separator, @header_items);
+
+  # 4. Write Line of Sight data:
+    # Go through the observations epochs:
+    for (my $i = 0; $i < scalar(@{ $ref_obs_data->{BODY} }); $i += 1)
+    {
+      # Epoch is transformed according to configuration:
+      my @epoch =
+         &{ $ref_epoch_sub }( $ref_obs_data->{BODY}[$i]{EPOCH} );
+
+      # Go trhough available iterations:
+      for my $iter (keys %{ $ref_obs_data->{BODY}[$i]{LSQ_INFO} }) {
+
+        # Save iteration data reference:
+        my $ref_iter_data = $ref_obs_data->{BODY}[$i]{LSQ_INFO}{$iter};
+
+        # Get iteration index:
+        # TODO? : change ICD in order to save iterations as a list?
+        my $iter_index = $1 if ( $iter =~ m/^ITER_(\d+)$/ );
+
+        # Save line items to print:
+        my @line_items = ( @epoch, $iter_index,
+                           $ref_iter_data->{STATUS},
+                           $ref_iter_data->{VARIANCE_ESTIMATOR}**(0.5),
+                           $ref_iter_data->{CONVERGENCE},
+                           @{ $ref_iter_data->{APX_PARAMETER} },
+                           @{ $ref_iter_data->{PARAMETER_VECTOR} } );
+
+        # Write LSQ line data:
+        say $fh join($separator, @line_items);
+
+      } # end for $iter
+    } # end for $i
+
+  # 5. Close dumper file:
+  close($fh);
+
+  # Sub's answer is TRUE if successfull:
+  return TRUE;
+}
+
+sub DumpSatPosition {
+  my ( $ref_dump_conf, $ref_gen_conf,
+       $ref_obs_data, $ref_sats_to_ignore, $output_path, $fh_log ) = @_;
+
+  # Default input values if not defined:
+  $fh_log = *STDOUT unless $fh_log;
+
+  # ************************* #
+  # Input consistency cehcks: #
+  # ************************* #
+
+  # Output path must exist and have write permissions:
+  unless (-w $output_path) {
+   RaiseError($fh_log, ERR_WRITE_PERMISSION_DENIED,
+     "User '".$ENV{USER}."' does not have write permissions at $output_path");
+   return KILLED;
+  }
+
+  # Dumper configuration must be hash type:
+  unless (ref($ref_dump_conf) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_dump_conf\' is not HASH type");
+   return KILLED;
+  }
+
+  # General configuration must be hash type:
+  unless (ref($ref_gen_conf) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_gen_conf\' is not HASH type");
+   return KILLED;
+  }
+
+  # Observation data must be hash type:
+  unless (ref($ref_obs_data) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_obs_data\' is not HASH type");
+   return KILLED;
+  }
+
+  # Satellites to discard must be array type:
+  unless (ref($ref_sats_to_ignore) eq 'ARRAY') {
+   RaiseError($fh_log, ERR_WRONG_ARRAY_REF,
+     "Input argument \'$ref_sats_to_ignore\' is not ARRAY type");
+   return KILLED;
+  }
+
+  # ********************************** #
+  # Satellite position dumper routine: #
+  # ********************************** #
+
+  # De-reference array inputs:
+  my @sats_to_ignore = @{ $ref_sats_to_ignore };
+
+  # Save dumper useful configuration:
+  my $separator          = $ref_dump_conf->{ SEPARATOR      };
+  my $ref_epoch_sub      = $ref_dump_conf->{ EPOCH_FORMAT   };
+  my $ref_sat_xyz_format = $ref_dump_conf->{ SAT_POS_FORMAT };
+
+  for my $sat_sys (@{ $ref_gen_conf->{SELECTED_SAT_SYS} })
+  {
+    # 1. Open dumper file at output path:
+      my $file_path = join('/', ($output_path, "$sat_sys-sat-xyz.out"));
+      my $fh; open($fh, '>', $file_path) or croak "Could not create $!";
+
+    # 2. Write title line:
+      # TODO: add observation epoch status info
+      say $fh sprintf("# > Satellite positions. Created  : %s \n".
+                      "#   Reference system for ECEF coordinates : %s \n",
+                      GetPrettyLocalDate(),
+                      $ref_gen_conf->{ELIPSOID});
+
+    # 3. Write header line:
+      my @header_items = qw( Epoch Status SatID
+                             SatPos[0] SatPos[1] SatPos[2] SatClockBias );
+
+      say $fh "#".join($separator, @header_items);
+
+    # 4. Write Line of Sight data:
+      # Go through the observations epochs:
+      for (my $i = 0; $i < scalar(@{ $ref_obs_data->{BODY} }); $i += 1)
+      {
+        # Save observation epoch status:
+        my $status =
+           $ref_obs_data->{BODY}[$i]{STATUS};
+        # Epoch is transformed according to configuration:
+        my @epoch =
+           &{ $ref_epoch_sub }( $ref_obs_data->{BODY}[$i]{EPOCH} );
+
+        # Go through available satellites in SAT_XYZTC hash:
+        for my $sat (keys %{ $ref_obs_data->{BODY}[$i]{SAT_XYZTC} })
+        {
+          # Ignore specified satellites:
+          unless (grep(/^$sat$/, @sats_to_ignore))
+          {
+            # Save satellite position data reference:
+            my $ref_sat_xyz_data = $ref_obs_data->{BODY}[$i]{SAT_XYZTC}{$sat};
+
+            # Satellite position is transformed according to configuration:
+            # TODO: Dump say XYZ and geodetic coordinates
+            my @sat_position =
+               &{ $ref_sat_xyz_format }( @{ $ref_sat_xyz_data->{NAV} } );
+
+            # Save line items:
+            my @line_items = ( @epoch, $status, $sat,
+                               @sat_position,
+                               $ref_sat_xyz_data->{NAV}[3] );
+
+            # Write data line:
+            # TODO: some uninitialized satellite position in GRPP verif/valid
+            say $fh join($separator, @line_items);
+
+          } # end unless $sat
+        } # end for $sat
+      } # end for $i
+
+    # 5. Close dumper file:
+    close($fh);
+
+  } # end for $sat_sys
+
+  # Sub's answer is TRUE if successfull:
+  return TRUE;
+}
+
+sub DumpRecPosition {
+  my ( $ref_dump_conf, $ref_gen_conf,
+       $ref_obs_data, $output_path, $fh_log ) = @_;
+
+  # Default input values if not defined:
+  $fh_log = *STDOUT unless $fh_log;
+
+  # ************************* #
+  # Input consistency cehcks: #
+  # ************************* #
+
+  # Output path must exist and have write permissions:
+  unless (-w $output_path) {
+   RaiseError($fh_log, ERR_WRITE_PERMISSION_DENIED,
+     "User '".$ENV{USER}."' does not have write permissions at $output_path");
+   return KILLED;
+  }
+
+  # Dumper configuration must be hash type:
+  unless (ref($ref_dump_conf) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_dump_conf\' is not HASH type");
+   return KILLED;
+  }
+
+  # General configuration must be hash type:
+  unless (ref($ref_gen_conf) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_gen_conf\' is not HASH type");
+   return KILLED;
+  }
+
+  # Observation data must be hash type:
+  unless (ref($ref_obs_data) eq 'HASH') {
+   RaiseError($fh_log, ERR_WRONG_HASH_REF,
+     "Input argument \'$ref_obs_data\' is not HASH type");
+   return KILLED;
+  }
+
+  # ************************************** #
+  # Receiver-Satellite LoS dumper routine: #
+  # ************************************** #
+
+  # Retrieve dumper configuration:
+  my $separator          = $ref_dump_conf->{ SEPARATOR      };
+  my $ref_epoch_sub      = $ref_dump_conf->{ EPOCH_FORMAT   };
+  my $ref_angle_sub      = $ref_dump_conf->{ ANGLE_FORMAT   };
+  my $sigma_factor       = $ref_dump_conf->{ SIGMA_FACTOR   };
+  my $ref_rec_xyz_format = $ref_dump_conf->{ REC_POS_FORMAT };
+
+  # 1. Open dumper file at output path:
+    my $file_path = join('/', ($output_path, "rec-xyz.out"));
+    my $fh; open($fh, '>', $file_path) or croak "Could not create $!";
+
+  # 2. Write title line:
+    say $fh sprintf("# > Receiver Position. Created : %s \n",
+                    GetPrettyLocalDate());
+
+  # 3. Write header line:
+    my @header_items = qw( Epoch Status
+                           RecPosX RecPosY RecPosZ RecClkBias
+                           RecSigmaX RecSigmaY RecSigmaZ SigmaClkBias
+                           RecLat RecLon RecElipHeight );
+
+    say $fh "#".join($separator, @header_items);
+
+  # 4. Write Receiver position data:
+    for (my $i = 0; $i < scalar(@{ $ref_obs_data->{BODY} }); $i += 1)
+    {
+      # Save reference to receiver position hash:
+      my $ref_rec_xyz_data =
+         $ref_obs_data->{BODY}[$i]{POSITION_SOLUTION};
+
+      # Epoch is transformed according to configuration:
+      my @epoch =
+         &{ $ref_epoch_sub }( $ref_obs_data->{BODY}[$i]{EPOCH} );
+
+      # Receiver position is transformed according to configuration:
+      my @rec_xyz =
+         @{ $ref_rec_xyz_data->{XYZDT} };
+
+      # Apply sigma factor:
+      my @rec_xyz_sigma =
+         map {$_*$sigma_factor} @{ $ref_rec_xyz_data->{SIGMA_XYZDT} };
+
+      # Geodetic receiver coordinates:
+      my ($rec_lat, $rec_lon, $rec_helip) =
+         ECEF2Geodetic( @rec_xyz[0..2], $ref_gen_conf->{ELIPSOID} );
+
+      # Latitude & Longitude are transformed according to configuration:
+      my @geodetic_angles =
+         &{ $ref_angle_sub }( $rec_lat, $rec_lon );
+
+      # Set data items:
+      my @line_items = ( @epoch, $ref_rec_xyz_data->{STATUS},
+                         @rec_xyz, @rec_xyz_sigma,
+                         @geodetic_angles, $rec_helip );
+
+      # Write data line:
+      say $fh join($separator, @line_items);
+
+    } # end for $i
+
+  # 5. Close dumper file:
+  close($fh);
+
+  # Sub's answer is TRUE if successfull:
+  return TRUE;
+}
 
 
 TRUE;
