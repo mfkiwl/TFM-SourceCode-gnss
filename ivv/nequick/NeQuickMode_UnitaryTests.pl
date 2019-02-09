2117b0a3856323af07bf624ab239a8bfd0e2b0a3
unknown <pintosantospablo@gmail.com>
2019-02-09 15:06:34 +0100

    Adding validation script for NeQuickMode GRPP package

---
+++ ivv/nequick/NeQuickMode_UnitaryTests.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/perl -w
+
+# Built-in modules:
+# ---------------------------------------------------------------------------- #
+use Carp;
+use strict;
+use Data::Dumper;
+use feature qq(say);
+
+# Script timestamps and memory usage:
+use Memory::Usage;
+use Time::HiRes qw(gettimeofday tv_interval);
+
+# Init script clock:
+  my $script_start = [gettimeofday];
+
+# Init memory usage report:
+  our $MEM_USAGE = Memory::Usage->new();
+      $MEM_USAGE->record('-> Init');
+
+# Load enviroments:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ ENV_ROOT };
+use Enviroments qq(:CONSTANTS);
+
+  my $timestamp_after_import_env = [gettimeofday];
+  $MEM_USAGE->record('-> Import Enviroments');
+
+# Load NeQuickMode Module:
+# ---------------------------------------------------------------------------- #
+use lib GRPP_ROOT_PATH;
+use NeQuickMode qq(:ALL);
+
+# Measure time to import NeQuickMode module:
+  my $timestamp_after_import_nequick = [gettimeofday];
+  $MEM_USAGE->record('-> Import NeQuickMode');
+
+# Load common libraries:
+# ---------------------------------------------------------------------------- #
+use lib LIB_ROOT_PATH;
+use MyUtil   qq(:ALL); # useful subs and constants...
+use MyMath   qq(:ALL); # useful mathematical methods...
+use MyPrint  qq(:ALL); # print error and warning methods...
+use TimeGNSS qq(:ALL); # GNSS time transforming utilities...
+use Geodetic qq(:ALL); # geodetic toolbox for coordinate transformation...
+
+# Measure time to import library modules:
+  my $timestamp_after_import_lib = [gettimeofday];
+  $MEM_USAGE->record('-> Import Library Modules');
+
+
+# Validation routine:
+# ---------------------------------------------------------------------------- #
+PrintTitle0(*STDOUT, "Launching validation script $0");
+
+# Dumper print of CCIR hash:
+  PrintTitle1(*STDOUT, "Testing Load of CCIR files:");
+  for (my $k = 0; $k < 12; $k += 1) {
+    print "K -> $k\n";
+    for (my $i = 0; $i < 2; $i += 1) {
+      for my $j (0..1) {
+        print REF_CCIR_HASH->{1}{F2}[$i][$j][$k]." ";
+      }
+      print " ... ";
+      for my $j (-2..-1) {
+        print REF_CCIR_HASH->{1}{F2}[$i][$j][$k]." ";
+      }
+      say " --> ".scalar( @{ REF_CCIR_HASH->{1}{F2}[$i] });
+    }
+    print "\n\n";
+  }
+  say "";
+
+# Dumper print of MODIP array:
+  PrintTitle1(*STDOUT, "Testing Load of MODIP file:");
+  for (my $i = 0; $i < scalar(@{ &REF_MODIP_MAP }); $i += 1) {
+    for my $j (0..4) {
+      print sprintf("%5.3f", REF_MODIP_MAP->[$i][$j]), " ";
+    }
+    print "... ";
+    for my $j (-5..-1) {
+      print sprintf("%5.3f", REF_MODIP_MAP->[$i][$j]), " ";
+    }
+    print "--> ".scalar(@{ REF_MODIP_MAP->[$i] }), "\n";
+  }
+  say "";
+
+# MODIP computation:
+  PrintTitle1(*STDOUT, "Testing ComputeMODIP() public sub:");
+
+  my ($test_lat, $test_lon) = ( 39.25, 3.145 );
+
+  my ( $modip ) = ComputeMODIP( $test_lat*DEGREE_TO_RADIANS,
+                                     $test_lon*DEGREE_TO_RADIANS );
+
+  PrintComment(*STDOUT,
+    "MODIP (lat = $test_lat; lon = $test_lon) = ".$modip*RADIANS_TO_DEGREE);
+
+# Report time stamps:
+  ReportElapsedTime($timestamp_after_import_nequick, $timestamp_after_import_env,
+    "Import NeQuickMode");
+
+  ReportElapsedTime($timestamp_after_import_lib, $timestamp_after_import_nequick,
+    "Import library modules");
+
+# Dumper memory usage report:
+  PrintTitle2(*STDOUT, 'Memory Usage report:');
+  $MEM_USAGE->dump(); say "";
+
+
+PrintTitle0(*STDOUT, "Validation script $0 is over");
+# end of script
+
+# Private subroutines:
+# ---------------------------------------------------------------------------- #
+sub ReportElapsedTime {
+  my ($current_time_stamp, $ref_time_stamp, $label) = @_;
+
+  say "";
+    PrintTitle2( *STDOUT, sprintf("Elapsed time for $label %.2f seconds",
+                          tv_interval($ref_time_stamp, $current_time_stamp)) );
+  say "";
+}
