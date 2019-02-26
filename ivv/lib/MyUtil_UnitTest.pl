acdaa51fd01fd5e6fdafb5b5ceabadb22525a692
ppinto94 <pintosantospablo@gmail.com>
2019-02-26 18:36:07 +0100

    Validation test for PushUnique with several values

--- ivv/lib/MyUtil_UnitTest.pl
+++ ivv/lib/MyUtil_UnitTest.pl
@@ -24,7 +24,19 @@ for ('a'..'f') {
 
 }
 
+my $ref_array2 = [1,2,5,8,10];
+
+say "Before loop:";
+print Dumper $ref_array2;
+say "--------------------------------------------------------------";
+
+PushUnique($ref_array2, (1..12));
+
+say "After pushing ".join(', ', (1..12)).":";
+print Dumper $ref_array2;
+
+
 my %hash;
 my $scalar;
 sub code {};
-PushUnique( \&code, 'A' );
+PushUnique( \&code, 'A', 'B' );
