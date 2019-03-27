012c6e26e3e058c1ac1aa60737c809bf39b2a3df
ppinto94 <pintosantospablo@gmail.com>
2019-03-27 17:45:32 +0100

    Adding script for validating IGS files reading routines

---
+++ ivv/rinex/ReadIGSPreciseProducts_Test.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -X
+
+use Carp;
+use strict;
+
+use Data::Dumper;
+use feature qq(say);
+
+# Load bash enviroments:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ ENV_ROOT };
+use Enviroments qq(:CONSTANTS);
+
+# Load common libraries:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ LIB_ROOT };
+use MyUtil qq(:ALL);
+use MyPrint qq(:ALL);
+use TimeGNSS qq(:ALL);
+
+# Load GRPP enviroments:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ GRPP_ROOT };
+use RinexReader qq(:ALL);
+
+# ============================================================================ #
+# Test Area
+# ============================================================================ #
+
+my $igs_dat_path = qq(/home/ppinto/WorkArea/dat/igs/);
+my $igs_sp3_file = qq(COD0MGXFIN_20183350000_01D_05M_ORB.SP3);
+
+my $ref_precise_orbit =
+  ReadPreciseOrbitIGS( join('/', ($igs_dat_path, $igs_sp3_file)), *STDOUT );
+
+print Dumper $ref_precise_orbit;
+
+
+
+
+
+
+
+
+
+# end of script
