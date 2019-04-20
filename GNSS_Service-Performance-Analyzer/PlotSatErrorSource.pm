0ed35d38f09e8ac20d3b568f1309b29de7055244
ppinto <pintosantospablo@gmail.com>
2019-04-20 13:13:49 +0200

    Adding common module and package declaration for GSPA modules

---
+++ GNSS_Service-Performance-Analyzer/PlotSatErrorSource.pm
@@ -0,0 +1,96 @@
+#!/usr/bin/perl -w
+
+# TODO: Package description goes here...
+
+# Package declaration:
+package PlotSatErrorSource;
+
+# Set package exportation properties:
+# ---------------------------------------------------------------------------- #
+BEGIN {
+  # Load export module:
+  require Exporter;
+
+  # Set package version:
+  our $VERSION = 1.0;
+
+  # Inherit from Exporter to export subs and constants:
+  our @ISA = qq(Exporter);
+
+  # Default export:
+  our @EXPORT = ();
+
+  # Define constants to export:
+  our @EXPORT_CONST = qw(  );
+
+  # Define subroutines to export:
+  our @EXPORT_SUB   = qw(  );
+
+  # Merge constants and subroutines:
+  our @EXPORT_OK = (@EXPORT_CONST, @EXPORT_SUB);
+
+  # Define export tags:
+  our %EXPORT_TAGS = ( ALL         => \@EXPORT_OK,
+                       DEFAULT     => \@EXPORT,
+                       CONSTANTS   => \@EXPORT_CONST,
+                       SUBROUTINES => \@EXPORT_SUB );
+}
+
+# Import common perl modules:
+# ---------------------------------------------------------------------------- #
+use Carp;         # advanced warning and failure raise...
+use strict;       # strict syntax and common mistakes advisory...
+
+use Data::Dumper;       # var pretty print...
+use feature qq(say);    # print adding line jump...
+use feature qq(switch); # advanced switch statement...
+
+# Load special tool modules:
+# ---------------------------------------------------------------------------- #
+# Perl Data Language (PDL) modules:
+use PDL;
+use PDL::NiceSlice;
+use Math::Trig qq(pi);
+
+# Perl-Gnuplot conection module:
+use Chart::Gnuplot;
+
+# Load bash enviroments:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ ENV_ROOT };
+use Enviroments qq(:CONSTANTS);
+
+# Load dedicated libraries:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ LIB_ROOT };
+use MyUtil   qq(:ALL); # ancillary utilities...
+use MyMath   qq(:ALL); # dedicated math toolbox...
+use MyPrint  qq(:ALL); # plain text print layouts...
+use TimeGNSS qq(:ALL); # GNSS time conversion tools...
+use Geodetic qq(:ALL); # dedicated geodesy utilities...
+
+# Load general configuration and interfaces module:
+# ---------------------------------------------------------------------------- #
+use lib $ENV{ SRC_ROOT };
+use GeneralConfiguration qq(:ALL);
+
+# Also, load GRPP's DataDumper constants
+# (needed for NULL_DATA):
+use lib $ENV{ GRPP_ROOT };
+use DataDumper qq(:CONSTANTS);
+
+# ---------------------------------------------------------------------------- #
+# Constants:
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Subroutines:
+# ---------------------------------------------------------------------------- #
+
+# Public Subroutines: #
+# ............................................................................ #
+
+# Private Subroutines: #
+# ............................................................................ #
+
+TRUE;
